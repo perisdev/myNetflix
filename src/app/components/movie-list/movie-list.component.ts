5a74e0771cf8641be4a39c1271e5a938f4f159a2
Santi Peris <perisdev>
2019-11-29 12:50:47 +0100

    added basic horizontal scroll control

--- src/app/components/movie-list/movie-list.component.ts
+++ src/app/components/movie-list/movie-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { Movie } from 'src/app/models/movie.model';
 import { MovieService } from 'src/app/services/movie.service';
 import { ActivatedRoute } from '@angular/router';
@@ -11,11 +11,21 @@ import { Genre } from 'src/app/models/genre.model';
 })
 export class MovieListComponent implements OnInit {
 
+  // to select a DOM elements.
+  @ViewChild('eleMovies', { read: ElementRef, static: false })
+  eleMovies: any
+
+  @ViewChild('eleWrap', { read: ElementRef, static: false })
+  eleWrap: any
+  //..
+
   listType: String;
   movies: Array<Movie> = [];
   genres: Array<Genre> = [];
 
   constructor(private route: ActivatedRoute, private movieService: MovieService) { }
+
+  margin: number = 0;
 
   ngOnInit() {
 
@@ -25,5 +35,36 @@ export class MovieListComponent implements OnInit {
         err => this.movies = []
       );
     });
+  }
+
+  wheelControl(e: any) {
+    // console.log((e.deltaY < 0) ? 'up' : 'down');
+
+    let moviesWidth = this.eleMovies.nativeElement.offsetWidth;
+    let wrapWidth = this.eleWrap.nativeElement.offsetWidth;
+    let diffMoviesWrap = (moviesWidth - wrapWidth) * -1;
+
+    // console.log(e); // remove me
+    // console.log(moviesWidth); // remove me
+    // console.log(wrapWidth); // remove me
+
+    // up to left
+    if (e.deltaY < 0 || e.deltaX < 0) {
+      this.margin += 30;
+
+      if (this.margin > 0)
+        this.margin = 0;
+
+      // down to right
+    } else if (e.deltaY >= 0 || e.deltaX >= 0) {
+      this.margin -= 30;
+
+      if (this.margin < diffMoviesWrap)
+        this.margin = diffMoviesWrap;
+    }
+
+    setTimeout(() => {
+      this.eleMovies.nativeElement.style = `margin-left: ${this.margin}px`;
+    }, 0);
   }
 }
